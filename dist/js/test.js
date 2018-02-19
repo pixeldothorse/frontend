9e39b0f371d3d39a96a8217e95025c548a312843
Eliot Partridge <shortcircuit369@gmail.com>
2018-02-18 21:20:02 -0600

    cleaner build setup, def. temporary
    todo: make this un-bad

--- dist/js/test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Test;
-(function (Test) {
-    test: (function () {
-        console.log("Hello!");
-    });
-})(Test = exports.Test || (exports.Test = {}));
-//# sourceMappingURL=test.js.map
